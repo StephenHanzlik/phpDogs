abcdf147dd58a348846a4c6c1cbbebe67f65a37c
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-24 12:40:40 -0600

    inital commit all dogs are good to go

---
+++ dogs.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Dogs in the House</title>
+    </head>
+	<body>
+      <p>
+      <?php
+        //Creating a class and adding properties
+        class Dog {
+            public $isHome;
+            public $name;
+            public $age;
+            public $isFed;
+
+            public function __construct($isHome, $name, $age, $isFed) {
+              $this->isHome = $isHome;
+              $this->name = $name;
+              $this->age = $age;
+              $this->isFed = $isFed;
+            }
+
+            // Adding a method to check the status of the dog
+            public function howAreYouDog() {
+             $fedOrNotString = "I dont want my dog food but I'll have whatever you are eating.</br>";
+             if($this->isFed === false){
+               $fedOrNotString = "and I am hungry!</br>";
+             }
+             if($this->isHome === true){
+               return "Woof, I am home and my name is " . $this->name . ".  I am " . $this->age . " years old " . $fedOrNotString;
+             }
+             if($this->isHome === false){
+               return "Woof, I am NOT home.  My name is " . $this->name . ".  I am " . $this->age . " years old and " . $fedOrNotString;
+             }
+           }
+          }
+
+        // Create new dogs
+        $jazz = new Dog(true, 'Jazz', 3, false);
+        $egg = new Dog(false, 'Egg', 2, false);
+        $dinky = new Dog(true, 'Dinky', 3, true);
+
+        // Print out how the dogs are doing
+        echo $jazz->howAreYouDog();
+        echo $egg->howAreYouDog();
+        echo $dinky->howAreYouDog();
+        ?>
+        </p>
+    </body>
+</html>
